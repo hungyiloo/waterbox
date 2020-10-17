15916037ae55f1c05bd3cfb1af407553e813f55f
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2020-10-17 11:35:58 +0800

    Demo app tidying and clarification

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,17 @@ export async function main() {
   // Set up the database
   const { db, query } = await initDatabase();
 
-  // A method to render the data from above onto the page
-  async function renderFruits(condition?: string) {
+  // Main render method
+  async function renderFruits(filterCondition?: string) {
+
     // Fetch some data from the database
-    const fruits = query(`SELECT * FROM fruits WHERE ${condition || '1=1'}`);
+    const fruits = query(`SELECT * FROM fruits WHERE ${filterCondition || '1=1'}`);
+
     // Render the template main.mustache into #main-container
     await renderContainer(
-      '#main-container',    // Find this id inside .  /index.html
-      'main.mustache',      // Find this template in ./templates/main.mustache
-      { fruits, condition } // The data we fetched above and the query condition that we last used
+      '#main-container',          // Find this id inside ./index.html
+      'main.mustache',            // Find this template in ./templates/main.mustache
+      { fruits, filterCondition } // The data we fetched above and the query condition that we last used
     );
 
     // Some example DOM event handlers.
@@ -25,10 +27,9 @@ export async function main() {
     const conditionInput = document.querySelector('#condition-input') as HTMLInputElement;
     const queryButton = document.querySelector('#query-btn') as HTMLButtonElement;
 
-    const queryHandler = async (_event: Event) => {
-      const condition = conditionInput ? conditionInput.value : null;
-      // Re-render with a condition in the where clause
-      await renderFruits(condition)
+    const queryHandler = async (_: Event) => {
+      const newFilterCondition = conditionInput ? conditionInput.value : null;
+      await renderFruits(newFilterCondition)
     };
     queryButton.onclick = queryHandler;
     conditionInput.onkeypress = async (event: KeyboardEvent) => {
